7e4521d3716661b6c85d3e235300322a3d4a440f
JHerculesqz <JHercules_qz@qq.com>
2016-11-03 12:11:31 +0800

    update build system

--- demo/lib/walle/js/walle.js
+++ demo/lib/walle/js/walle.js
@@ -1,9 +1,14 @@
 function Core1(){
 	this.name="Core1";
-};
+    var privateProp = "privateProp";
 
-Core1.prototype.hello = function(){
-	console.log("hello," + this.name);
+    this.prototype.hello = function(){
+        console.log("hello," + this.name);
+    };
+
+    var privateFunc = function(){
+        console.log("privateFunc");
+    };
 };
 
 function Component1(){
--- demo/walle/src/core/core1/js/core1.js
+++ demo/walle/src/core/core1/js/core1.js
@@ -1,7 +1,12 @@
 function Core1(){
 	this.name="Core1";
-};
+    var privateProp = "privateProp";
 
-Core1.prototype.hello = function(){
-	console.log("hello," + this.name);
+    this.prototype.hello = function(){
+        console.log("hello," + this.name);
+    };
+
+    var privateFunc = function(){
+        console.log("privateFunc");
+    };
 };
